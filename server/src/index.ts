dabe1ac41a1e6bcd00ee52fa6f145a8ad1279e21
RehaS <beratson@gmail.com>
2018-08-20 11:48:21 +0200

    Server running, tsconfig, compile and serve watchers up

---
+++ server/src/index.ts
@@ -0,0 +1,58 @@
+import 'reflect-metadata'
+import { Action, BadRequestError, useKoaServer } from 'routing-controllers'
+// import setupDb from './db'
+// import { verify } from './jwt'
+import * as Koa from 'koa'
+import {Server} from 'http'
+// import {secret} from './jwt'
+
+const app = new Koa()
+const server = new Server(app.callback())
+const port = process.env.PORT || 4000
+
+useKoaServer(app, {
+  cors: true,
+  controllers: [
+    
+  ],
+  // authorizationChecker: (action: Action) => {
+  //   const header: string = action.request.headers.authorization
+  //   if (header && header.startsWith('Bearer ')) {
+  //     const [ , token ] = header.split(' ')
+
+  //     try {
+  //       return !!(token && verify(token))
+  //     }
+  //     catch (e) {
+  //       throw new BadRequestError(e)
+  //     }
+  //   }
+
+  //   return false
+  // },
+  // currentUserChecker: async (action: Action) => {
+  //   const header: string = action.request.headers.authorization
+  //   if (header && header.startsWith('Bearer ')) {
+  //     const [ , token ] = header.split(' ')
+      
+  //     if (token) {
+  //       const {id} = verify(token)
+  //       return User.findOneById(id)
+  //     }
+  //   }
+  //   return undefined
+  // }
+})
+
+
+
+
+
+
+
+// setupDb()
+//   .then(_ => {
+    server.listen(port)
+    console.log(`Succesfully Listening on port ${port}`)
+  // })
+  // .catch(err => console.error(err))
