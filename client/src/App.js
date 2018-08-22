9be5eb1424a514f79af3ba69cc1a78e13cb2a15e
RehaS <beratson@gmail.com>
2018-08-22 16:55:23 +0200

    FrontEnd startPoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
@@ -13,6 +14,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Router>
+          <main>
+            Start  
+          </main>
+        </Router> 
       </div>
     );
   }
