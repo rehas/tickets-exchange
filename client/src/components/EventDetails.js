3cf305751a5905a8f1a080b22f2bf3575b8bd0ff
RehaS <beratson@gmail.com>
2018-08-23 02:53:46 +0200

    Risk calculation api implemented, Ticket list view implemented

--- client/src/components/EventDetails.js
+++ client/src/components/EventDetails.js
@@ -1,5 +1,5 @@
 import React,{PureComponent} from 'react';
-import {Grid} from '@material-ui/core'
+import {Grid, Paper, Table, TableHead, TableRow, TableCell, TableBody} from '@material-ui/core'
 import {connect} from 'react-redux'
 import { withStyles } from '@material-ui/core/styles';
 import {getEvent} from '../actions/events'
@@ -7,6 +7,14 @@ import { Link } from 'react-router-dom'
 
 
 const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
   container: {
     display: 'block',
     flexWrap: 'wrap',
@@ -41,10 +49,51 @@ class EventDetails extends PureComponent  {
   }
 
   render(){
-    return (<div>
-       This is event :  {this.props.match.params.eventid}
+    return (<div>{
+      this.props.event &&
+    
+       <Paper className={this.props.classes.root}>
+      <Table className={ this.props.classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Name</TableCell>
+            <TableCell >Description</TableCell>
+            <TableCell numeric>Price</TableCell>
+            <TableCell numeric>Risk</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {this.props.event.tickets.map(ticket => {
+            return (
+                
+              <TableRow key={ticket.id}>
+                <TableCell component="th" scope="ticket">
+                  {ticket.event_id}
+                </TableCell>
+                <TableCell >{ticket.description}</TableCell>
+                <TableCell numeric>{ticket.price}</TableCell>
+                <TableCell numeric>{ticket.risk}</TableCell>
+                <TableCell >
+                  <Link to={`/tickets/${ticket.id}`}>
+                  Ticket Details
+                  </Link>
+                </TableCell>
+
+              </TableRow>
+            );
+          })}
+        </TableBody>
+      </Table>
+    </Paper>
+    }
        </div> )
   }
 }
 
-export default connect(null, {getEvent})(withStyles(styles)(EventDetails));
+const mapStateToProps = state =>{
+  return {
+    event : state.eventDetails
+  }
+}
+
+export default connect(mapStateToProps, {getEvent})(withStyles(styles)(EventDetails));
--- server/src/tickets/controller.ts
+++ server/src/tickets/controller.ts
@@ -107,4 +107,61 @@ export default class TicketController {
 
     return event
   }
+
+  @Get('/tickets/:ticketid([0-9]+)/risk')
+  async getTicketRisk(
+    @Param('ticketid') ticketid: number,
+  ){
+    const ticket = await Ticket.findOneById(ticketid, {relations:["comments"]})
+    if(!ticket) throw new NotFoundError("Ticket Not Found")
+    console.log(ticket)
+    // console.log(ticket.event)
+    // console.log(ticket.user)
+    const author = await User.findOneById(ticket.user_id, {relations:["tickets"]})
+    const event = await Event.findOneById(ticket.event_id, {relations:["tickets"]})
+
+    if(!author) throw new NotFoundError("Author Not Found")
+    if(!event) throw new NotFoundError("Event Not Found")
+
+    let risk =2;
+
+    if (author.tickets.length === 1) {risk +=4}
+
+    const averagePrice = event.tickets.reduce((agg : number, val : Ticket)=>{
+        return agg=val.price + agg
+      }, 0) / event.tickets.length ;
+
+    const priceDiff = ((ticket.price - averagePrice) / averagePrice)*100
+    
+
+    if (priceDiff < 0){
+      risk += Math.abs(priceDiff)
+    }else{
+      (priceDiff > 15) ? risk-=15 : risk-=priceDiff
+    }
+    console.log(risk, "risk after average price calc")
+
+    const hour = parseInt( ticket.createdat.toString().split(' ')[4].split(':')[0]) +2
+
+    console.log(hour)
+
+    if(hour > 9 && hour < 17){
+      risk -=13
+    }else{
+      risk+=13
+    }
+
+    if (ticket.comments.length > 3){
+      risk +=6
+    }
+
+    console.log(risk)
+
+    risk = (risk < 2) ? 2 : (risk >98) ? 98 : risk
+
+    return {ticket, 
+    risk: risk}
+
+  }
+
 }
