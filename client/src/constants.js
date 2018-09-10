e23596a37bc62a5584e911db644c3541954273f3
RehaS <beratson@gmail.com>
2018-09-10 13:26:25 +0200

    heroku env variable added

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:4000'
+export const baseUrl = process.env.REACT_ENV_STATUS=== 'production' ? 'https://ebay-for-tickets-api.herokuapp.com/' : 'http://localhost:4000'
 // export const baseUrl = 'http://172.16.30.241:4000'
 
 export const localStorageJwtKey = 'currentUserJwt'
